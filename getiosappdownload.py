08248f3d20ab608d1b7ff941324bc2c2d4e98d7c
dacaiguoguo <dacaiguoguo@163.com>
2024-05-11 15:54:51 +0800

    Initial commit

--- getiosappdownload.py
+++ getiosappdownload.py
@@ -2,6 +2,8 @@ import requests
 import re
 import json
 import subprocess
+import zipfile
+import os
 
 def extract_id_from_url(url):
     """ 从给定的URL中提取以 'id' 开始的应用ID """
@@ -22,14 +24,45 @@ def lookup_app_by_id(app_id):
 
 def download_app(bundle_id):
     """ 使用ipatool下载应用 """
+    ipa_filename = f"{bundle_id}.ipa"
     try:
-        subprocess.run(["ipatool", "download", "-b", bundle_id], check=True)
+        subprocess.run(["ipatool", "download", "-b", bundle_id, "-o", ipa_filename], check=True)
         print("App download initiated successfully.")
+        return ipa_filename
     except subprocess.CalledProcessError as e:
         print(f"Failed to download the app: {e}")
+        return None
+
+def unzip_ipa(ipa_filename):
+    """ 解压IPA文件并找到.app目录 """
+    if not ipa_filename or not os.path.exists(ipa_filename):
+        print("No IPA file found to unzip.")
+        return None
+
+    extract_folder = "extracted_ipa"
+    with zipfile.ZipFile(ipa_filename, 'r') as zip_ref:
+        zip_ref.extractall(extract_folder)
+    
+    # 寻找 .app 文件夹
+    for root, dirs, files in os.walk(extract_folder):
+        for dir in dirs:
+            if dir.endswith(".app"):
+                app_path = os.path.join(root, dir)
+                print(f"Found .app folder: {app_path}")
+                return app_path
+    return None
+
+def rename_app_folder(app_path):
+    """ 移除.app扩展名并重命名文件夹 """
+    if app_path and app_path.endswith(".app"):
+        new_path = app_path[:-4]  # 移除".app"
+        os.rename(app_path, new_path)
+        print(f"Renamed {app_path} to {new_path}")
+        return new_path
+    return app_path
 
 def main():
-    url = "https://apps.apple.com/us/app/photoroom-%E7%BC%96%E8%BE%91%E7%85%A7%E7%89%87%E5%92%8C%E8%83%8C%E6%99%AF/id1455009060?l=zh-Hans-CN&ppid=d2ffc899-92a4-47c6-bbf4-cfadbea6ddb2"
+    url = "https://apps.apple.com/us/app/驴妈妈旅游-特价订酒店旅游景点门票/id443926246"
     app_id = extract_id_from_url(url)
     print(f"Extracted App ID: {app_id}")
     
@@ -37,7 +70,15 @@ def main():
     print(f"Bundle ID: {bundle_id}")
 
     # 执行下载
-    download_app(bundle_id)
+    ipa_filename = download_app(bundle_id)
+    
+    # 解压IPA文件
+    app_path = unzip_ipa(ipa_filename)
+    
+    # 重命名.app文件夹
+    if app_path:
+        new_app_path = rename_app_folder(app_path)
+        # 在这里你可以对新的文件夹进行进一步的处理
 
 if __name__ == "__main__":
     main()
